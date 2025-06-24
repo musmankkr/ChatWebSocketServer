11946bbc940003c88512bac12cfc24b6b05e9613
Usman Khokhar <usman.khokhar@wavetec.com>
2025-06-24 14:38:02 +0500

    updated key

--- Services/DialogflowService.cs
+++ Services/DialogflowService.cs
@@ -1,6 +1,7 @@
 ﻿using ChatWebSocketServer.Configurations;
 using Google.Cloud.Dialogflow.V2;
 using Microsoft.Extensions.Options;
+using System.IO;
 using Environment = System.Environment;
 
 namespace ChatWebSocketServer.Services;
@@ -9,11 +10,17 @@ public class DialogflowService
 {
     private readonly DialogflowSettings _settings;
 
-    public DialogflowService(IOptions<DialogflowSettings> options)
+    public DialogflowService(IOptions<DialogflowSettings> options, IWebHostEnvironment env)
     {
         _settings = options.Value;
 
-        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", _settings.CredentialsPath);
+        var fullPath = Path.Combine(env.ContentRootPath, _settings.CredentialsPath);
+        //Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", _settings.CredentialsPath);
+
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", fullPath);
+        var path = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+        Console.WriteLine($"Auth file path: {path}");
+        Console.WriteLine($"Exists: {File.Exists(path)}");
     }
 
     public async Task<string> DetectIntentAsync(string userInput)
